5390ebe3670887c44d17999c64216cd7a845fe25
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-08-03 23:11:25 -0300

    style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Footer, Navbar } from '@/components'
-import './globals.css'
 import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Car Hub',
